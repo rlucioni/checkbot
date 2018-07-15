243459810e6f3395f319ef45b2532619cc2520dc
Renzo Lucioni <renzo@lucioni.xyz>
2018-07-15 18:50:09 -0400

    implement checkbot

--- checkbot.py
+++ checkbot.py
@@ -1,6 +1,93 @@
+import logging
+import os
+from logging.config import dictConfig
+
+import requests
+from redis import StrictRedis
+from slackclient import SlackClient
+
+
+dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '{asctime} {levelname} {process} [{filename}:{lineno}] - {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+})
+
+logger = logging.getLogger(__name__)
+
+
+HMART_NUMBER = os.environ.get('HMART_NUMBER')
+HMART_NAME = os.environ.get('HMART_NAME')
+HMART_ZIP = os.environ.get('HMART_ZIP')
+HMART_POINTS_URL = 'http://scpoint.hmart.com/Controller-DMZPointInquiry/jsp/smc-dmz-get-cust-point.jsp'
+
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
+REDIS_POINTS_KEY = 'points'
+
+SLACK_API_TOKEN = os.environ.get('SLACK_API_TOKEN')
+SLACK_CHANNEL = '#checkbot'
+
+
 class Checkbot:
     def __init__(self):
-        pass
+        self.redis = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+        self.slack = SlackClient(SLACK_API_TOKEN)
+
+    def check(self):
+        form = {
+            'custno': HMART_NUMBER,
+            'lastname': HMART_NAME,
+            'zipcode': HMART_ZIP,
+        }
+
+        response = requests.post(HMART_POINTS_URL, data=form)
+        data = response.json()
+        points = data['tpldata'][0]['point']
+        date = data['tpldata'][0]['trdate']
+
+        cached = self.redis.get(REDIS_POINTS_KEY)
+        is_point_change = int(points) != int(cached) if cached else False
+
+        if not cached or is_point_change:
+            self.redis.set(REDIS_POINTS_KEY, points)
+
+            # TODO: include difference between old and new totals in message?
+            self.message(f'{points} Hmart points as of {date}')
+
+    def message(self, message):
+        self.slack.api_call(
+            'chat.postMessage',
+            channel=SLACK_CHANNEL,
+            text=message,
+        )
+
+
+def check():
+    try:
+        Checkbot().check()
+    except:
+        logger.exception('something went wrong')
 
 
 def exception_handler(*args, **kwargs):
@@ -9,4 +96,4 @@ def exception_handler(*args, **kwargs):
 
 
 if __name__ == '__main__':
-    Checkbot().check()
+    check()
--- test_checkbot.py
+++ test_checkbot.py
@@ -1,25 +1,102 @@
+import urllib.parse
+from unittest.mock import patch
+
 import pytest
 import responses
 from redis import StrictRedis
 
-from checkbot import Checkbot
+from checkbot import (
+    HMART_POINTS_URL,
+    SLACK_CHANNEL,
+    check,
+)
 
 
+SLACK_MESSAGE_URL = 'https://slack.com/api/chat.postMessage'
+
+
+@patch('checkbot.HMART_NUMBER', '123')
+@patch('checkbot.HMART_NAME', 'foo')
+@patch('checkbot.HMART_ZIP', '98110')
+@patch('checkbot.SLACK_API_TOKEN', 'fake-token')
 class TestCheckbot:
     @pytest.fixture(autouse=True)
     def flush_redis(self):
         StrictRedis().flushall()
 
+    def mock_slack(self):
+        responses.add(
+            responses.POST,
+            SLACK_MESSAGE_URL,
+            json={'ok': True},
+        )
+
+    def assert_calls(self, *urls):
+        assert len(responses.calls) == len(urls)
+
+        for index, url in enumerate(urls):
+            assert responses.calls[index].request.url == url
+
+    def assert_slack(self, call, message):
+        body = dict(urllib.parse.parse_qsl(call.request.body))
+        assert body['channel'] == SLACK_CHANNEL
+        assert body['text'] == message
+
+    def reset_calls(self):
+        responses.calls.reset()
+
     @responses.activate
-    def test_check_points(self):
-        pass
-        # responses.add(
-        #     responses.GET,
-        #     self.today_url,
-        #     match_querystring=True,
-        #     json=schedule.serialized(),
-        # )
+    def test_check(self):
+        body = {
+            'tpldata': [
+                {
+                    'custno': '123',
+                    'custname': 'foo',
+                    'trdate': '07/14/2018',
+                    'point': '182',
+                    'isdeleted': '0',
+                },
+            ],
+        }
 
-        # poll()
+        responses.add(
+            responses.POST,
+            HMART_POINTS_URL,
+            json=body,
+        )
 
-        # self.assert_calls(self.yesterday_url, self.today_url)
+        self.mock_slack()
+
+        check()
+
+        self.assert_calls(HMART_POINTS_URL, SLACK_MESSAGE_URL)
+        self.assert_slack(responses.calls[1], '182 Hmart points as of 07/14/2018')
+        self.reset_calls()
+
+        check()
+
+        self.assert_calls(HMART_POINTS_URL)
+        self.reset_calls()
+
+        new_body = {
+            'tpldata': [
+                {
+                    'custno': '123',
+                    'custname': 'foo',
+                    'trdate': '07/15/2018',
+                    'point': '213',
+                    'isdeleted': '0',
+                },
+            ],
+        }
+
+        responses.replace(
+            responses.POST,
+            HMART_POINTS_URL,
+            json=new_body,
+        )
+
+        check()
+
+        self.assert_calls(HMART_POINTS_URL, SLACK_MESSAGE_URL)
+        self.assert_slack(responses.calls[1], '213 Hmart points as of 07/15/2018')
