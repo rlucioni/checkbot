77acce6d2e41a94fbe6d36df8d3ff2c23f49f55f
Renzo Lucioni <renzo@lucioni.xyz>
2018-10-13 16:37:41 -0400

    ez login

--- checkbot.py
+++ checkbot.py
@@ -1,8 +1,10 @@
+import json
 import logging
 import os
 from logging.config import dictConfig
 
 import requests
+from bs4 import BeautifulSoup
 from redis import StrictRedis
 from slackclient import SlackClient
 
@@ -39,6 +41,9 @@ HMART_NUMBER = os.environ.get('HMART_NUMBER')
 HMART_NAME = os.environ.get('HMART_NAME')
 HMART_ZIP = os.environ.get('HMART_ZIP')
 HMART_POINTS_URL = 'http://scpoint.hmart.com/Controller-DMZPointInquiry/jsp/smc-dmz-get-cust-point.jsp'
+
+EZ_USERNAME = os.environ.get('EZ_USERNAME')
+EZ_PASSWORD = os.environ.get('EZ_PASSWORD')
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
@@ -96,10 +101,49 @@ def check():
         logger.exception('something went wrong')
 
 
+def login():
+    base_url = 'https://www.ezdrivema.com'
+    session = requests.Session()
+    session.headers.update({
+        'User-Agent': (
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/69.0.3497.100 Safari/537.36'
+        )
+    })
+
+    login_url = f'{base_url}/ezpassmalogin'
+
+    response = session.get(login_url)
+
+    if response.status_code == 200:
+        logger.info('login get ok')
+    else:
+        logger.info('login get failed')
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    with open('forms/login.json') as f:
+        form = json.load(f)
+
+    form['__RequestVerificationToken'] = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
+    form['__VIEWSTATE'] = soup.find(id='__VIEWSTATE').get('value')
+    form['__EVENTVALIDATION'] = soup.find(id='__EVENTVALIDATION').get('value')
+    form['dnn$ctr689$View$txtUserName'] = EZ_USERNAME
+    form['dnn$ctr689$View$txtPassword'] = EZ_PASSWORD
+
+    response = session.post(login_url, data=form)
+
+    if response.status_code == 200:
+        logger.info('login post ok')
+    else:
+        logger.info('login post failed')
+
+
 def exception_handler(*args, **kwargs):
     # prevents invocation retry
     return True
 
 
 if __name__ == '__main__':
-    check()
+    # check()
+    login()
